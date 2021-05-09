d45c22eab0546aebe42842387634cd00d0525b89
Sudhir Verma <sudhirvvermaa@gmail.com>
2021-05-10 00:57:58 +0530

    code spliting

--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { render } from "react-dom";
-import SearchParams from "./SearchParams";
-import { Router } from "@reach/router";
-import Details from "./Details";
+// import SearchParams from "./SearchParams";
+import { Link, Router } from "@reach/router";
 import ThemeContext from "./ThemeContext";
-import NavBar from "./NavBar";
+import ErrorBoundary from "./ErrorBoundary";
+// import NavBar from "./NavBar";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const themeHook = useState("darkblue");
@@ -12,19 +15,25 @@ const App = () => {
     // <React.StrictMode>
     <ThemeContext.Provider value={themeHook}>
       <div>
-        <NavBar />
-        {/* <header>
-            <Link to="/">Adopt Me!</Link>
-          </header> */}
-
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
+        {/* <NavBar /> */}
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Suspense fallback={<h1>loading route …</h1>}>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
     // </React.StrictMode>
   );
 };
 
-render(<App />, document.getElementById("root"));
+render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
+  document.getElementById("root")
+);
